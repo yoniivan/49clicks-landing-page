e2492a8a960b135b03f5621256b8c4127cbaadf7
yoni1 <yoni12284@hotmail.com>
2020-02-25 18:52:43 +0200

    second commit

--- logic.js
+++ logic.js
@@ -1,1 +1,20 @@
-// alert("working");
+
+
+window.onload = function() {
+    
+    const screenSize = window.innerWidth;
+
+    if (screenSize > 850){
+        const small = document.getElementById("small");
+        small.style.display = 'none'
+    }
+
+
+
+
+    else{
+        const big = document.getElementById("big")
+        big.style.display = 'none';
+    }
+}
+
